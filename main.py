81ff26ac35cb12d8f39aa39255505f28f4088a13
Lucas Demasi <ldemasi@student.saintaug.NSW.edu.au>
2024-05-21 10:13:33 +1000

    have responses and checker in the code just need it to display on the open window

--- main.py
+++ main.py
@@ -2,6 +2,82 @@ import gooeypie as gp
 #imports all the classes downloaded via the
 # pip
 
+
+def on_text_change(event):
+    text = text_box.text
+    print(text)
+
+
+    if len(text) < 10:
+        print("This code it strong")
+    else:
+        print("This password needs some strengthening")
+
+app = gp.GooeyPieApp('Hello!, I am a password Checker')
+
+text_box = gp.Textbox(app)
+text_box.add_event_listener('change', on_text_change)
+
+label = gp.Label(app, 'Type in your password to be checked')
+
+app.width = 300
+app.height = 200
+app.title = "Password Checker"
+# sets the width attributes to 1000
+
+# set up Grid
+app.set_grid(2, 1)
+app.add(text_box,1,1, align='center')
+app.add(label,2,1, align='center')
+    
+
+    
+
+def toggle_mask(event):
+    secret.toggle()
+    text = text_box.text
+    print(text)
+
+    
+    if len(text) < 10:
+        print("This code it strong")
+    else:
+        print("This password needs some strengthening")
+
+app = gp.GooeyPieApp('Password checker')
+
+question = gp.Label(app, "What's your password?")
+
+# This is where the users password is.
+secret = gp.Secret(app)
+secret.width = 50
+
+# This makes the code visible
+check = gp.Checkbox(app, '🪬 Show password')
+check.add_event_listener('change', toggle_mask)
+
+# setup of the grid
+app.set_grid(3, 1)
+app.add(question, 1, 1)
+app.add(secret, 2, 1)
+app.add(check, 3, 1)
+
+app.run()
+
+# Need to have a password checker that will read the passwords
+# symbols and if it doesn't have enough figures (8-10) it is
+# a weak password. Then provide a option to the slider for a 
+# more secure password.
+
+# put a slider in that will provide auto generated password 
+# that is stronger or weaker passcode based on there choice.
+
+
+
+
+
+
+# SCRAPPED CODE:
 # def on_text_change(event):
 #     text = text_box.text
 #     print(text)
@@ -55,25 +131,3 @@ import gooeypie as gp
 # # put stuff in Grid!!!
 # app.add(btn,1,1, align='center')
 # app.add(lbl,2,1, align='center')
-
-def toggle_mask(event):
-    secret.toggle()
-
-app = gp.GooeyPieApp('Secret')
-
-question = gp.Label(app, "What's your password?")
-
-secret = gp.Secret(app)
-secret.width = 50
-
-check = gp.Checkbox(app, 'Show password')
-check.add_event_listener('change', toggle_mask)
-
-# setup of the grid
-app.set_grid(3, 1)
-app.add(question, 1, 1)
-app.add(secret, 2, 1)
-app.add(check, 3, 1)
-
-app.run()
-
